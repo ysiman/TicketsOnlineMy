02edafb53ed351fd9fdb0530ae82b96ca70f01bf
ysiman <isiman@yandex.ru>
2020-12-28 18:22:31 +0300

    Реализованы сами классы трансформеров (объекты для преобразовывания   сущностей и DTO)

---
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/dto/base/BaseDTO.java
@@ -0,0 +1,42 @@
+package org.itsimulator.germes.app.rest.dto.base;
+
+import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
+
+/**
+ * Base class for all DTO classes
+ * 
+ * @author Morenets
+ *
+ */
+public abstract class BaseDTO<T extends AbstractEntity> {
+	/**
+	 * Unique entity identifier
+	 */
+	private int id;
+
+	/**
+	 * Should be overridden in the derived classes if additional transformation
+	 * logic domain model -> DTO is needed.
+	 * Overridden methods should call super.transform()
+	 */
+	public void transform(T t) {
+		id = t.getId();
+	}
+
+	/**
+	 * Should be overridden in the derived classes if additional transformation
+	 * logic DTO -> domain model is needed
+	 */
+	public T untransform(T t) {
+		t.setId(getId());
+		return t;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
